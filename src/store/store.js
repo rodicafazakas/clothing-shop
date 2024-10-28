a1101aec72f85026242586dbc7ec381ea8628f8a
rodica <11833084+rodicafazakas@users.noreply.github.com>
2024-10-28 15:49:23 +0100

    persist reducer values in local storage

--- src/store/store.js
+++ src/store/store.js
@@ -1,16 +1,25 @@
 import { compose, createStore, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 import rootReducer from './rootReducer';
+
+const persistConfig = {
+  key: 'root',
+  storage, 
+  blacklist: ['user'], // because user value is coming from the auth state listener
+};
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const middlewares = [logger];
 
 const composedEnhancers = compose(applyMiddleware(...middlewares));
 
-const store = createStore(
-    rootReducer,
+export const store = createStore(
+    persistedReducer,
     undefined,
     composedEnhancers
   );
 
-export default store;
+export const persistor = persistStore(store);
