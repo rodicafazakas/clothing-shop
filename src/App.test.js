d728bb4f49f7ef4872dedc8e3f095ae7eec37da7
rodica <11833084+rodicafazakas@users.noreply.github.com>
2024-11-12 10:24:49 +0100

    convert px to rem

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,18 @@
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import { render, screen } from '@testing-library/react';
+
+import store from './store/store';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+test('renders contact link', () => {
+  render(
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>  
+  );
+  const linkElement = screen.getByText(/CONTACT/i);
   expect(linkElement).toBeInTheDocument();
 });
